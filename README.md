05292078596d21ff83df58162fab6cab93cf0b38
UnsignedByte <edlam1889@gmail.com>
2025-01-29 17:50:59 -0500

    lesson 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Structure
+
+This repository contains work from [CS 6120](https://www.cs.cornell.edu/courses/cs6120/2025sp/). Lesson tasks and work can be found in the [lessons](lessons/) folder. Other miscellaneous things (mostly testing/for fun) are found in [playground](playground/).
---
+++ lessons/2/insn-counter.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+import argparse
+import subprocess
+import json
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='BRIL Program Instruction Counter')
+    parser.add_argument('--file', required=True, help='Path to the BRIL program')
+    parser.add_argument('--args', nargs='*', help='Arguments to the BRIL program')
+    parser.add_argument('--output', help='Path to the output file')
+
+    args = parser.parse_args()
+
+    print("Reading file: {}".format(args.file))
+
+    with open(args.file, 'r') as f:
+        # if the file ends with .bril (instead of .json)
+        # run bril2json on the file
+        if args.file.endswith('.bril'):
+            bril2json = subprocess.run(['bril2json'], stdin=f, stdout=subprocess.PIPE)
+            data = bril2json.stdout.decode('utf-8')
+            program = json.loads(data)
+        else:
+            program = json.load(f)
+
+    # loop through all the functions in the program
+    for func in program['functions']:
+        new_insns = [
+            {
+                "dest": "__insn_count",
+                "op": "const",
+                "type": "int",
+                "value": 0
+            },
+            {
+                "dest": "__insn_one",
+                "op": "const",
+                "type": "int",
+                "value": 1
+            }
+        ]
+        for instr in func['instrs']:
+            if "label" in instr:
+                # If the instruction is a label, pass
+                new_insns.append(instr)
+                continue
+            # Add an instruction to increment the instruction count
+            new_insns.append({
+                "dest": "__insn_count",
+                "op": "add",
+                "type": "int",
+                "args": ["__insn_count", "__insn_one"]
+            })
+
+            if instr['op'] == 'ret':
+                # If the instruction is a return instruction
+                # Print the instruction count
+                new_insns.append({
+                    'op': 'print',
+                    'args': ['__insn_count']
+                })
+
+            # Add the original instruction
+            new_insns.append(instr)
+        # Also print at the very end of the function in case there is no return
+        new_insns.append({
+            'op': 'print',
+            'args': ['__insn_count']
+        })
+
+        func['instrs'] = new_insns
+            
+                
+    # Write the json to the output file
+    if args.output:
+        with open(args.output, 'w') as f:
+            json.dump(program, f, indent=2)
+    
+    # Now run brili on the modified program
+    program = json.dumps(program)
+    subprocess.run(['brili', '-p', *args.args], input=program.encode('utf-8'))
+
+
